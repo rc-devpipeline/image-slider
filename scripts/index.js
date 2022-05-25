4f852b27b742aadfb40e381f40f7e7ab77c2a072
semperry <ryan.curtis88@gmail.com>
2022-05-25 14:26:05 -0600

    adds image swipe functionality

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,8 @@
-console.log("Hello script");
+const slider = document.getElementById("slider");
+
+slider.addEventListener("input", (e) => {
+	const sliderValue = e.target.value;
+	const img = document.querySelector(".foreground-img");
+
+	img.style.width = `${sliderValue}%`;
+});
